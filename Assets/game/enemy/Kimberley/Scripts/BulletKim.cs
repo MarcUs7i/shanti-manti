470364d8dc79598ed8a883db9c971e835903e1d5
Kizuren <96580944+Kizuren@users.noreply.github.com>
2024-10-10 08:59:12 +0200

    Refactored Kimberley Enemy

--- Assets/game/enemy/Kimberley/Scripts/BulletKim.cs
+++ Assets/game/enemy/Kimberley/Scripts/BulletKim.cs
@@ -6,20 +6,13 @@ public class BulletKim : MonoBehaviour
 {
     public float speed = 20f;
     public int damage = 40;
-    public Rigidbody2D rb;
     public GameObject impactEffect;
+    Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
     {
-        if (Kimberley.BulletKimDirection == 0f)
-        {
-            rb.velocity = -transform.right * speed;
-        }
-        if (Kimberley.BulletKimDirection == 1f)
-        {
-            rb.velocity = transform.right * speed;
-        }
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * Kimberley.BulletKimDirection * speed;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
