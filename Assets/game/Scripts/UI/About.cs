a222aad8923cc43944ba33383bcc6819759edd0b
Kizuren <96580944+Kizuren@users.noreply.github.com>
2024-12-28 19:29:36 +0100

    Refactored and set defaults to scene defaults in About.cs

--- Assets/game/Scripts/UI/About.cs
+++ Assets/game/Scripts/UI/About.cs
@@ -7,82 +7,63 @@ using UnityEngine.SceneManagement;
 
 public class About : MonoBehaviour
 {
-    //public TMPro yourText;
-    //public float speed = 25f;
-    public bool move = false;
-    float time = 0f;
-    public float wait1 = 68.0f;
-    public float wait2 = 85.0f;
+    public bool move;
+    private float _time;
+    public float wait1 = 46.0f;
+    public float wait2 = 55.0f;
 
     public SceneFader sceneFader;
     public string levelToLoad = "Start";
 
     public RectTransform obj;
 
-    public float moveSpeed = 100.0f; // Adjust this to control the speed of movement
+    public float moveSpeed = 5.0f; // Adjust this to control the speed of movement
     public float movePercentage = 0.02f; // Adjust this to control the distance moved based on screen height
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        StartCoroutine(UP());
+        StartCoroutine(Up());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Calculate the new Y position based on the screen height and movePercentage.
         float screenHeight = Screen.height;
         float newY = screenHeight * movePercentage;
 
-        if (time == 2)
+        if (Mathf.Approximately(_time, 2))
         {
             sceneFader.FadeTo(levelToLoad);
         }
-        if (move == true)
+        if (move)
         {
             // Calculate the new position based on time and moveSpeed
-            Vector3 newPosition = transform.position + new Vector3(0, newY, 0) * moveSpeed * Time.deltaTime;
+            Vector3 newPosition = transform.position + new Vector3(0, newY, 0) * (moveSpeed * Time.deltaTime);
 
             // Update the UI element's position
             transform.position = newPosition;
         }
     }
 
-    IEnumerator UP()
+    private IEnumerator Up()
     {
         yield return new WaitForSeconds(5.0f);
         move = true;
         yield return new WaitForSeconds(wait1);
         move = false;
         obj.localPosition = new Vector3(0f,-500f,0f);
-        time++;
-        StartCoroutine(SecondUP());
+        _time++;
+        StartCoroutine(SecondUp());
     }
-    IEnumerator SecondUP()
+
+    private IEnumerator SecondUp()
     {
         move = true;
         yield return new WaitForSeconds(wait2);
         move = false;
         obj.localPosition = new Vector3(0f,-500f,0f);
-        time++;
-        StartCoroutine(SecondUP());
+        _time++;
+        StartCoroutine(SecondUp());
     }
-
-    /*public float moveSpeed = 100.0f; // Adjust this to control the speed of movement
-    public float movePercentage = 0.02f; // Adjust this to control the distance moved based on screen height
-
-    void Update()
-    {
-        // Calculate the new Y position based on the screen height and movePercentage.
-        float screenHeight = Screen.height;
-        float newY = screenHeight * movePercentage;
-
-        // Calculate the new position based on time and moveSpeed
-        Vector3 newPosition = transform.position + new Vector3(0, newY, 0) * moveSpeed * Time.deltaTime;
-
-        // Update the UI element's position
-        transform.position = newPosition;
-    }*/
 }
